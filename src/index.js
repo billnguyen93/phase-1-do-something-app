6f28e993f30b413b7dc943d5b6926e36df080183
Bill Nguyen <billfl93@gmail.com>
2022-08-30 23:54:18 -0400

    changed HTML homepage styling

--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
 document.addEventListener('DOMContentLoaded', () => {
     getActivity()
+    showActivity()
 })
 
 const boredUrl = "http://www.boredapi.com/api/activity/"
+
+//DOM Selectors
+
+const mainDiv = document.querySelector("#main")
+
+//Rendering
+
+function showActivity() {
+    const h3 = document.createElement('h3')
+    h3.innerText = "Pick Something to Do"
+    mainDiv.appendChild(h3)
+    h3.style.marginTop = "0"
+}
 
 function getActivity() {
     fetch("http://www.boredapi.com/api/activity/")
 .then(resp => resp.json())
 .then(activity => console.log(activity))
 }
+
