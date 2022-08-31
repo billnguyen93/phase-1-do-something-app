cdc83773f60d8281d32e3e0beee111ce9c0df2a5
Bill Nguyen <billfl93@gmail.com>
2022-08-30 23:35:43 -0400

    added DOMContentLoaded and fetch request

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-alert('hi')
+document.addEventListener('DOMContentLoaded', () => {
+    getActivity()
+})
+
+const boredUrl = "http://www.boredapi.com/api/activity/"
+
+function getActivity() {
+    fetch("http://www.boredapi.com/api/activity/")
+.then(resp => resp.json())
+.then(activity => console.log(activity))
+}
