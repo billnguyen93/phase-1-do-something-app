c5ddbc58a3c066738a94e0faf8978fa29b42cee9
Bill Nguyen <billfl93@gmail.com>
2022-09-01 00:16:38 -0400

    created buttons

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     getActivity() 
-    homePage()
+    
 })
 
 const boredUrl = "http://www.boredapi.com/api/activity/"
@@ -13,37 +13,23 @@ const button = document.querySelector('#reload-activity-button')
 //Rendering
 
 function getActivity() {
-    fetch("http://www.boredapi.com/api/activity/")
+ return fetch("http://www.boredapi.com/api/activity/")
 .then(resp => resp.json())
 .then(activity => showActivity(activity))
 }
 
 function showActivity(activity) {
     
-    const h4 = document.getElementById("show-activity")
-    h4.innerText = activity.activity
-     
+    const h6 = document.getElementById("show-activity")
+    h6.innerText = activity.activity
+    const priceLi = document.getElementById("price")
+    priceLi.innerText = "Price: " + activity.price 
+    const participantsLi = document.getElementById("participants")
+    participantsLi.innerText = "Participants: " + activity.participants
+
    }
 
-
-
-
-function resetMainDiv() {
-    mainDiv.innerHTML = ''
-}
-
-function homePage() {
-    const h1 = document.createElement('h1')
-    h1.innerText = "Choose An Activity"
-    const p = document.createElement('p')
-    p.innerText = "Click on Get Activity to generate a random activity to do"
-
-    mainDiv.append(h1, p)
-}
-
 //Event Listeners
-
-button.addEventListener('click', showActivity)
 
 //Event Handlers
 
