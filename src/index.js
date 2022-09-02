da3a1c2f0dc1624ecc104604f85312254f1dfbf4
Bill Nguyen <billfl93@gmail.com>
2022-09-02 01:13:08 -0400

    changed font and added my activities card

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 document.addEventListener('DOMContentLoaded', () => {
-    getActivity() 
+    getActivity()
+   
     
 })
 
 const boredUrl = "http://www.boredapi.com/api/activity/"
 
 //DOM Selectors
-
+const bodyDiv = document.querySelector("#activity")
+const mainDiv = document.querySelector("#main")
+const formDiv = document.querySelector('#form-container')
+const button = document.querySelector('#new-activity')
 const participantsLi = document.getElementById("participants")
 const h6 = document.getElementById("show-activity")
 const priceLi = document.getElementById("price")
-const bodyDiv = document.querySelector("#activity")
-const button = document.querySelector('#new-activity')
+
+
 
 //Rendering
 
@@ -31,6 +35,7 @@ function getActivity() {
 
 //Event Listeners
 button.addEventListener('click', getActivity)
+
 
 //Event Handlers
 
