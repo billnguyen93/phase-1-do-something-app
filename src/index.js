1a3ba4db74e2a1c2c873c31c5b34b5b2443d984d
Bill Nguyen <billfl93@gmail.com>
2022-09-06 14:42:01 -0400

    add remove function

--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
     getActivity()
     submitActivity()
-    
+    removeActivity()
 })
 
 const boredUrl = "http://www.boredapi.com/api/activity/"
 
 //DOM Selectors
+
 const activityDiv = document.querySelector("#my-activities")
 const bodyDiv = document.querySelector("#activity")
 const mainDiv = document.querySelector("#main")
 const formDiv = document.querySelector('#form-container')
 
 const button = document.querySelector('#new-activity')
-const likeBtn = document.querySelector('#like-button')
+
 
 const participantsLi = document.getElementById("participants")
 const h6 = document.getElementById("show-activity")
@@ -28,40 +29,54 @@ const activityForm = document.querySelector("#activity-form")
 function getActivity() {
     return fetch(boredUrl)
         .then(resp => resp.json())
-        .then(data => {
-            
-            currentActivity = data.key
-            const activity = data.activity
-            h6.innerText = activity
-            const price = data.price
-            priceLi.innerText = "Price: " + price
-            const participants = data.participants
-            participantsLi.innerText = "Participants: " + participants
-            likeBtn.addEventListener('click', () => {  
-                let p = document.createElement("p")
-                p.innerText = data.activity          
-                likedActivities.appendChild(p)
-                })
-        })
+        .then(data => renderActivity(data))
+}
+
+function renderActivity(data) {
+    const activity = data.activity
+    h6.innerText = activity
+    const price = data.price
+    priceLi.innerText = "Price: " + price
+    const participants = data.participants
+    participantsLi.innerText = "Participants: " + participants
+    
+    const likeBtn = document.getElementById('like-button')
+    likeBtn.addEventListener('click', handlelikeBtn)
+    
 }
 
 function submitActivity() {
     activityForm.addEventListener('submit', (e) => {
-        e.preventDefault()
-        handleActivity(e.target.newActivity.value)
-    })
+    e.preventDefault()
+    handleActivity(e.target.newActivity.value)  
+    document.getElementById("activity-form").reset()
+  })
+    
 }
 
+function removeActivity() {
+    p.innerHTML = ''
+    
+}
 
 //Event Listeners
+
 button.addEventListener('click', getActivity)
+p.addEventListener('click', removeActivity)
+
 
 //Event Handlers
+
 function handleActivity(activity) {
-    let p = document.createElement("p")
     p.textContent = `${activity}`
     likedActivities.appendChild(p)
     
+}
+
+function handlelikeBtn(data) {
+    p.innerText = data.activity          
+    likedActivities.append(p)
+    console.log(data)
 }
 
 
